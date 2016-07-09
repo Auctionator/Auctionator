3eb7b58980490dbee132b7036c252c21a6eb765d
John Roberts <roberts.john@gmail.com>
2016-07-09 02:51:59 -0700

    Hrm

--- spec/temp_spec.lua
+++ spec/temp_spec.lua
@@ -1,5 +1,36 @@
-describe( "TODO: Write tests", function()
-  it( "should be done", function()
-    assert.truthy( "Yup." )
+describe("Busted unit testing framework", function()
+  describe("should be awesome", function()
+    it("should be easy to use", function()
+      assert.truthy("Yup.")
+    end)
+
+    it("should have lots of features", function()
+      -- deep check comparisons!
+      assert.are.same({ table = "great"}, { table = "great" })
+
+      -- or check by reference!
+      assert.are_not.equal({ table = "great"}, { table = "great"})
+
+      assert.truthy("this is a string") -- truthy: not false or nil
+
+      assert.True(1 == 1)
+      assert.is_true(1 == 1)
+
+      assert.falsy(nil)
+      assert.has_error(function() error("Wat") end, "Wat")
+    end)
+
+    it("should provide some shortcuts to common functions", function()
+      assert.are.unique({{ thing = 1 }, { thing = 2 }, { thing = 3 }})
+    end)
+
+    it("should have mocks and spies for functional tests", function()
+      local thing = require("thing_module")
+      spy.on(thing, "greet")
+      thing.greet("Hi!")
+
+      assert.spy(thing.greet).was.called()
+      assert.spy(thing.greet).was.called_with("Hi!")
+    end)
   end)
-)
+end)
