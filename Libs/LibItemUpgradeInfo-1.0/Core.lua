205da27c9f79cc873b5a4563c77cdc786182403a
John Roberts <roberts.john@gmail.com>
2017-08-31 19:35:43 -0700

    libitem version bump

--- Libs/LibItemUpgradeInfo-1.0/Core.lua
+++ Libs/LibItemUpgradeInfo-1.0/Core.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = "LibItemUpgradeInfo-1.0", 28
+local MAJOR, MINOR = "LibItemUpgradeInfo-1.0", 29
 
 local type,tonumber,select,strsplit,GetItemInfoFromHyperlink=type,tonumber,select,strsplit,GetItemInfoFromHyperlink
 
