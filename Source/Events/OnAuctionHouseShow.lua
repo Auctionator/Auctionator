2997d722b5ad03b86d1c78475c15d8e9d9293960
plusmouse <60280559+plusmouse@users.noreply.github.com>
2020-01-25 06:03:33 +0000

    Scan entire AH once every logon

--- Source/Events/OnAuctionHouseShow.lua
+++ Source/Events/OnAuctionHouseShow.lua
@@ -1,15 +1,20 @@
 function Auctionator.Events.OnAuctionHouseShow()
-  print("In OnAuctionHouseShow")
-  -- AuctionatorFrame:Show();
-
   Atr_OnAuctionHouseShow();
+  AutoScan();
+end
+
+function AutoScan()
+  --Autoscan once per logon
+  if (not Auctionator.InitialScanComplete) then
+      Auctionator.Search.Simple("");
+      Auctionator.InitialScanComplete = true;
+  end
 end
 
 -----------------------------------------
 
 function Atr_OnAuctionHouseShow()
-  Auctionator.Debug.Message( 'Atr_OnAuctionHouseShow' )
-
+  Auctionator.Debug.Message( 'Atr_OnAuctionHouseShow' );
   -- local frame = CreateFrame("Frame", "AuctionatorMain", AuctionHouseFrame, "SimplePanelTemplate");
   -- frame:SetPoint("TOPLEFT", AuctionHouseFrame, "TOPRIGHT", -2, -20)
   -- frame:SetPoint("BOTTOMLEFT", AuctionHouseFrame, "BOTTOMRIGHT", -2, 5)
