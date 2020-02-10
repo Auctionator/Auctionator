5af7ae36a43d5ea9f58f089d419a4e9a2cdbb760
plusmouse <60280559+plusmouse@users.noreply.github.com>
2020-02-10 14:21:20 +0000

    Remove local only file

--- scripts/local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-TARGET="/mnt/c/Program Files (x86)/World of Warcraft/_retail_/Interface/Addons/Auctionator"
-rm -r $TARGET
-rsync -avrq --progress ./ $TARGET --exclude scripts --exclude .git && echo "Synced" $(date) || echo "Fail"
