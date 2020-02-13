17ce4ad24e56fd44042102d74c429b7356bff8ad
plusmouse <60280559+plusmouse@users.noreply.github.com>
2020-02-13 21:56:05 +0000

    Last remaining splitting

--- Source/FullScan/QuickReplication.lua
+++ Source/FullScan/QuickReplication.lua
@@ -2,24 +2,16 @@ function Auctionator.FullScan.QuickReplication()
   local prices = {}
   for index = 0, C_AuctionHouse.GetNumReplicateItems() - 1 do
     local replicateItemInfo = {C_AuctionHouse.GetReplicateItemInfo(index)}
-    local name = replicateItemInfo[1]
     local count = replicateItemInfo[3]
     local buyoutPrice = replicateItemInfo[10]
-    local itemId = replicateItemInfo[17]
     local effectivePrice = buyoutPrice / count
-    local itemKey = tostring(itemId)
-    --Special case for pets in cages
-    if itemId==82800 then
-      if name~=nil then
-        local speciesId, _ = C_PetJournal.FindPetIDByName(name)
-        itemKey = "p:"..tostring(speciesId)
+    local itemKey = Auctionator.Utilities.ItemKeyFromReplicateResult(replicateItemInfo)
+    if itemKey~=nil then
+      if prices[itemKey] == nil then
+        prices[itemKey] = { effectivePrice }
+      else
+        table.insert(prices[itemKey], effectivePrice)
       end
-    end
-
-    if prices[itemKey] == nil then
-      prices[itemKey] = { effectivePrice }
-    else
-      table.insert(prices[itemKey], effectivePrice)
     end
   end
   Auctionator.Debug.Message("Auctionator.FullScan.QuickReplication", "Calling ProcessFullScan...")
---
+++ Source/Utilities/ItemKeyFromReplicate.lua
@@ -0,0 +1,14 @@
+function Auctionator.Utilities.ItemKeyFromReplicateResult(info)
+  local name = replicateItemInfo[1]
+  local itemId = replicateItemInfo[17]
+  --Special case for pets in cages
+  if itemId==82800 then
+    if name~=nil then
+      local speciesId, _ = C_PetJournal.FindPetIDByName(name)
+      return "p:"..tostring(speciesId)
+    else
+      return nil
+    end
+  else
+    return tostring(itemId)
+end
