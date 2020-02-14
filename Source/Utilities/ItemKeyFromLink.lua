150f91b38ff506df0c532b305d7c566b0cbbe13d
plusmouse <60280559+plusmouse@users.noreply.github.com>
2020-02-14 21:37:29 +0000

    Integrate fixes to #440 and #456

--- Source/Utilities/ItemKeyFromLink.lua
+++ Source/Utilities/ItemKeyFromLink.lua
@@ -1,9 +1,15 @@
 function Auctionator.Utilities.ItemKeyFromLink(itemLink)
-  local _, _, itemString = string.find(itemLink, "^|c%x+|H(.+)|h%[.*%]")
-  local linkType, itemId, _, _, _, _, _, _, _ = strsplit(":", itemString)
-  if linkType == "battlepet" then
-    return "p:"..itemId;
+  if itemLink ~= nil then
+    local _, _, itemString = string.find(itemLink, "^|c%x+|H(.+)|h%[.*%]")
+    local linkType, itemId, _, _, _, _, _, _, _ = strsplit(":", itemString)
+    if linkType == "battlepet" then
+      return "p:"..itemId;
+    elseif linkType == "item" then
+      return itemId;
+    else
+      return nil
+    end
   else
-    return itemId;
+    return nil
   end
 end
