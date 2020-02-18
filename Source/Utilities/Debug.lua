9c525470a47e122894ebf879422ef710d07c7e5e
plusmouse <60280559+plusmouse@users.noreply.github.com>
2020-02-18 19:31:29 +0000

    Missed a bit

--- Source/Utilities/Debug.lua
+++ Source/Utilities/Debug.lua
@@ -10,6 +10,5 @@ end
 function Auctionator.Debug.Message(message, ...)
   if Auctionator.Debug.IsOn() then
     print(GREEN_FONT_COLOR:WrapTextInColorCode(message), ...)
-    -- print( '|cff008000'..message..'|r', ... )
   end
 end
