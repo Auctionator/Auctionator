ee03b4cea6cf31be50da97f468bdce5d726035c5
plusmouse <60280559+plusmouse@users.noreply.github.com>
2020-02-29 01:37:39 +0000

    missed a ;

--- Source/Utilities/StringContains.lua
+++ Source/Utilities/StringContains.lua
@@ -1,10 +1,10 @@
 function Auctionator.Utilities.StringContains(s, sub, ...)
   if s == nil or sub == nil or sub == "" then
-    return false;
+    return false
   end
 
   if string.find(string.lower(s), string.lower(sub), 1, true) ~= nil then
-    return true;
+    return true
   end
 
   return Auctionator.Utilities.StringContains(s, ...);
