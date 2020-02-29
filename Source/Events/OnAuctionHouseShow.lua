3fd4440113815b49e039673fccd9af5470e042a5
John Roberts <roberts.john@gmail.com>
2020-02-28 16:43:02 -0800

    [Fixes #526] Only create one AuctionatorAH Frame (#531)
    * Also saves config settings on panel change

--- Source/Events/OnAuctionHouseShow.lua
+++ Source/Events/OnAuctionHouseShow.lua
@@ -1,6 +1,9 @@
 function Auctionator.Events.OnAuctionHouseShow()
   Auctionator.Debug.Message("Auctionator.Events.OnAuctionHouseShow()")
 
-  local frame = CreateFrame("FRAME", "AuctionatorAHFrame", AuctionHouseFrame, "AuctionatorAHFrameTemplate")
-  FrameUtil.RegisterFrameForEvents(frame, { "AUCTION_HOUSE_SHOW", "AUCTION_HOUSE_CLOSED"})
+  if Auctionator.State.AuctionatorFrame == nil then
+    Auctionator.State.AuctionatorFrame = CreateFrame("FRAME", "AuctionatorAHFrame", AuctionHouseFrame, "AuctionatorAHFrameTemplate")
+  end
+
+  FrameUtil.RegisterFrameForEvents(Auctionator.State.AuctionatorFrame, { "AUCTION_HOUSE_SHOW", "AUCTION_HOUSE_CLOSED"})
 end
