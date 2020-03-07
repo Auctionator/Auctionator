1eed3436b2912fd956e7b355ee16e3bddec9b022
plusmouse <plusmouse@protonmail.com>
2020-03-07 17:06:27 +0000

    Changed function used in Utilities.CreateMoneyString

--- Source/Utilities/CreateMoneyString.lua
+++ Source/Utilities/CreateMoneyString.lua
@@ -1,6 +1,3 @@
-local _, addonTable = ...;
-local zc = addonTable.zc;
-
 function Auctionator.Utilities.CreateMoneyString(count)
-  return zc.priceToMoneyString(count, true)
+  return GetCoinTextureString(count)
 end
