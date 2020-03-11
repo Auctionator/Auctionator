49a80849edad67acca1458021056d57147d06a7b
plusmouse <plusmouse@protonmail.com>
2020-03-11 17:57:33 +0000

    [Fixes #571] Resolving throttling issues due to the undercut mixins
    * "Post Item" button is greyed out if throttling would cause the auction
    to fail to post
    * Optimisated (removed) aggregation methods to get the lowest priced
    item/commodity

---
+++ Source/Utilities/ApplyThrottlingButton.lua
@@ -0,0 +1,6 @@
+function Auctionator.Utilities.ApplyThrottlingButton(button, isThrottling)
+  button:SetEnabled(not isThrottling)
+  if isThrottling then
+    button:SetTooltip("Throttled. Waiting for the server.")
+  end
+end
