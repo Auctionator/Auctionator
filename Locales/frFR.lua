58464622627c2742f604ee05a18b756762d16ed2
plusmouse <plusmouse@protonmail.com>
2020-04-04 19:31:45 +0100

    finale

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -1,6 +1,15 @@
 AUCTIONATOR_LOCALES.frFR = function()
   local L = {}
 
+  L["CONFIG_TOOLTIPS_CATEGORY"] = "Infobulles"
+  L["CONFIG_LIFO_CATEGORY"] = "Ventes"
+  L["DEFAULT_AUCTION_DURATION"] = "Durée des enchères par défaut"
+  L["AUCTION_DURATION_12"] = "12 Heures"
+  L["AUCTION_DURATION_24"] = "24 Heures"
+  L["AUCTION_DURATION_48"] = "48 Heures"
+
+  L["SALES_PREFERENCE"] = "Préférence de vente"
+
   --@localization(locale="frFR", format="lua_additive_table")@
 
   return L
--- Source/Locales/Main.lua
+++ Source/Locales/Main.lua
@@ -1,28 +1,32 @@
-ATR_L = {}
+local currentLocale = {}
 
-local function fixMissingTranslations(incomplete, locale)
+local function FixMissingTranslations(incomplete, locale)
   if locale == "enUS" then
     return
   end
   local enUS = AUCTIONATOR_LOCALES["enUS"]()
   for key, val in pairs(enUS) do
     if incomplete[key] == nil then
-      incomplete[key] = "XXX-"..key.."-XXX"
+      incomplete[key] = val
     end
   end
 end
 
 if AUCTIONATOR_LOCALES[GetLocale()] ~= nil then
-  ATR_L = AUCTIONATOR_LOCALES[GetLocale()]()
-  fixMissingTranslations(ATR_L, GetLocale())
+  currentLocale = AUCTIONATOR_LOCALES[GetLocale()]()
+  FixMissingTranslations(currentLocale, GetLocale())
 else
-  ATR_L = AUCTIONATOR_LOCALES["enUS"]()
+  currentLocale = AUCTIONATOR_LOCALES["enUS"]()
 end
 
-for key, value in pairs(ATR_L) do
+for key, value in pairs(currentLocale) do
   _G["AUCTIONATOR_L_"..key] = value
 end
 
 function Auctionator.Locales.Apply(s, ...)
-  return string.format(ATR_L[s] or s, ...)
+  if currentLocale[s] ~= nil then
+    return string.format(currentLocale[s], ...)
+  else
+    error("Unknown/missing locale string")
+  end
 end
