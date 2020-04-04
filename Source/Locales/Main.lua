c20409be2be8b4abcd544989470d6a610e0cd7e0
plusmouse <plusmouse@protonmail.com>
2020-04-04 19:36:43 +0100

    whitespace

--- Source/Locales/Main.lua
+++ Source/Locales/Main.lua
@@ -4,6 +4,7 @@ local function FixMissingTranslations(incomplete, locale)
   if locale == "enUS" then
     return
   end
+
   local enUS = AUCTIONATOR_LOCALES["enUS"]()
   for key, val in pairs(enUS) do
     if incomplete[key] == nil then
@@ -14,11 +15,13 @@ end
 
 if AUCTIONATOR_LOCALES[GetLocale()] ~= nil then
   currentLocale = AUCTIONATOR_LOCALES[GetLocale()]()
+
   FixMissingTranslations(currentLocale, GetLocale())
 else
   currentLocale = AUCTIONATOR_LOCALES["enUS"]()
 end
 
+-- Export constants into the global scope (for XML frames to use)
 for key, value in pairs(currentLocale) do
   _G["AUCTIONATOR_L_"..key] = value
 end
