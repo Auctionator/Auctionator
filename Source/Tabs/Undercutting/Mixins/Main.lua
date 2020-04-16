949fd2a3a732f55eb5f3d908f2fb0f41504dad45
John Roberts <roberts.john@gmail.com>
2020-04-16 12:41:47 -0700

    Hiding

--- Source/Tabs/Undercutting/Mixins/Main.lua
+++ Source/Tabs/Undercutting/Mixins/Main.lua
@@ -3,15 +3,3 @@ AuctionatorUndercuttingFrameMixin = {}
 function AuctionatorUndercuttingFrameMixin:OnLoad()
   Auctionator.Debug.Message("AuctionatorUndercuttingFrameMixin:OnLoad()")
 end
-
--- Just putting this here since its temporary
-AuctionatorMagicButton = {}
-
-function AuctionatorMagicButton:OnLoad()
-  DynamicResizeButton_Resize(self)
-end
-
-function AuctionatorMagicButton:OnClick()
-  Auctionator.Debug.Message("AuctionatorMagicButton:OnClick()")
-
-end
