f6f88a1578a4b5c707814e4558706c81121c388b
plusmouse <plusmouse@protonmail.com>
2020-04-18 22:35:16 +0100

    curseforge translations; remove all local translations

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -1,17 +1,6 @@
 AUCTIONATOR_LOCALES.frFR = function()
   local L = {}
 
-  --@debug@
-  L["CONFIG_TOOLTIPS_CATEGORY"] = "Infobulles"
-  L["CONFIG_LIFO_CATEGORY"] = "Ventes"
-  L["DEFAULT_AUCTION_DURATION"] = "Durée des enchères par défaut"
-  L["AUCTION_DURATION_12"] = "12 Heures"
-  L["AUCTION_DURATION_24"] = "24 Heures"
-  L["AUCTION_DURATION_48"] = "48 Heures"
-
-  L["SALES_PREFERENCE"] = "Préférence de vente"
-  --@end-debug@
-
   --@localization(locale="frFR", format="lua_additive_table")@
 
   return L
