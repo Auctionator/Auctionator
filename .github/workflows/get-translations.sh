f7e598102ea30f844ba83fc64fdea08336150186
plusmouse <plusmouse@protonmail.com>
2020-04-19 12:23:40 +0100

    Initial build chain for translations

---
+++ .github/workflows/get-translations.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+function get-translations(){
+    curl -H "X-Api-Token: 4b5c72e1-613d-4d0a-a8fc-a6cf6eebdc5c" -X GET -H \
+    "Content-type: application/json" \
+    >temp.lua \
+    "https://wow.curseforge.com/api/projects/6124/localization/translations?lang=$1"
+
+    sed -e '/--.*$/{r temp.lua' -e 'd}' Locales/$1.lua >temp2.lua
+    mv temp2.lua Locales/$1.lua
+    rm temp.lua
+}
+get-translations $1
+
