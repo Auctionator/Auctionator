1dbfc896de440faa0fbdf0f5861c0c1e5df7b38a
plusmouse <plusmouse@protonmail.com>
2020-04-19 15:30:51 +0100

    Modify build to import translations

--- .github/workflows/get-translations.sh
+++ .github/workflows/get-translations.sh
@@ -1,12 +1,12 @@
-#!/bin/zsh
+#!/bin/bash
 function get-translations(){
     curl -H "X-Api-Token: 4b5c72e1-613d-4d0a-a8fc-a6cf6eebdc5c" -X GET -H \
     "Content-type: application/json" \
     >temp.lua \
-    "https://wow.curseforge.com/api/projects/6124/localization/translations?lang=$1"
+    "https://wow.curseforge.com/api/projects/6124/localization/export?lang=$1"
 
-    sed -e '/--.*$/{r temp.lua' -e 'd}' Locales/$1.lua >temp2.lua
-    mv temp2.lua Locales/$1.lua
+    sed -e '/--.*$/{r temp.lua' -e 'd}' Auctionator/Locales/$1.lua >temp2.lua
+    mv temp2.lua Auctionator/Locales/$1.lua
     rm temp.lua
 }
 get-translations $1
