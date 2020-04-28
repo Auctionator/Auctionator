6d20f853717c79310d3bc90a0ed69bfbf625995a
plusmouse <plusmouse@protonmail.com>
2020-04-28 16:58:26 +0100

    Handle inserted newlines in curseforge translations

--- .github/workflows/get-translations.sh
+++ .github/workflows/get-translations.sh
@@ -6,8 +6,9 @@ function get-translations(){
     "https://wow.curseforge.com/api/projects/6124/localization/export?lang=$1"
 
     sed -e '/--.*$/{r temp.lua' -e 'd}' Auctionator/Locales/$1.lua >temp2.lua
-    mv temp2.lua Auctionator/Locales/$1.lua
-    rm temp.lua
+    awk '{gsub("\\\\\\\\n", "\\n", $0); print}' temp2.lua >temp.lua
+    mv temp.lua Auctionator/Locales/$1.lua
+    rm temp2.lua
 }
 get-translations $1
 
