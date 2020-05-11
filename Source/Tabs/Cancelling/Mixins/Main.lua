6f37a19f1b31fad25169bca911940451f3212f1d
plusmouse <plusmouse@protonmail.com>
2020-05-11 18:40:04 +0100

    Sound effect

--- Source/Tabs/Cancelling/Mixins/Main.lua
+++ Source/Tabs/Cancelling/Mixins/Main.lua
@@ -4,12 +4,16 @@ function AuctionatorCancellingFrameMixin:OnLoad()
   Auctionator.Debug.Message("AuctionatorCancellingFrameMixin:OnLoad()")
 
   self.ResultsListing:Init(self.DataProvider)
+
   Auctionator.EventBus:Register(self, {Auctionator.Cancelling.Events.RequestCancel})
 end
 
 function AuctionatorCancellingFrameMixin:ReceiveEvent(eventName, eventData, ...)
   if eventName == Auctionator.Cancelling.Events.RequestCancel then
     Auctionator.Debug.Message("Executing cancel request", eventData)
+
     C_AuctionHouse.CancelAuction(eventData)
+
+    PlaySound(SOUNDKIT.IG_MAINMENU_OPEN)
   end
 end
