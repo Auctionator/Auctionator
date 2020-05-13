cebebbbadbd79b64d8e8348691916ecafb1314b7
plusmouse <plusmouse@protonmail.com>
2020-05-13 09:23:20 +0100

    [Fixes #719] Better wording explaining the selling undercut options

--- Source/Components/Mixins/TitleFrame.lua
+++ Source/Components/Mixins/TitleFrame.lua
@@ -7,5 +7,9 @@ function AuctionatorConfigurationTitleFrameMixin:OnLoad()
 
   if self.subTitleText then
     self.SubTitle:SetText(self.subTitleText)
+
+    -- Width value doesn't matter, but setting this makes the word wrap work
+    -- The anchors in the frame xml set the actual width.
+    self.SubTitle:SetWidth(200)
   end
 end
