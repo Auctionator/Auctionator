f02166825270ef04cb29978c9c48b25b368b8d2f
plusmouse <plusmouse@protonmail.com>
2020-05-14 18:00:40 +0100

    Closure

--- Source/AH/Mixins/Queue.lua
+++ Source/AH/Mixins/Queue.lua
@@ -1,30 +1,34 @@
-Auctionator.AH.Queue = {}
+Auctionator.AH.Queue = (function()
+  local Mixin = {}
 
-function Auctionator.AH.Queue:Init()
-  self.queue = {}
-  Auctionator.EventBus:Register(self, {
-    --Auctionator.AH.Events.Ready
-    Auctionator.AH.Events.SearchReady
-  })
-end
-
-function Auctionator.AH.Queue:Push(func)
-  table.insert(self.queue, func)
-
-  if Auctionator.AH.internals.throttling.searchReady then
-    self:_Pop()
+  function Mixin:Init()
+    self.queue = {}
+    Auctionator.EventBus:Register(self, {
+      --Auctionator.AH.Events.Ready
+      Auctionator.AH.Events.SearchReady
+    })
   end
-end
 
-function Auctionator.AH.Queue:ReceiveEvent(event)
-  print(event)
-  self:_Pop()
-end
-
-function Auctionator.AH.Queue:_Pop()
-  if #self.queue > 0 then
-    print("calling")
-    Auctionator.AH.internals.throttling:Call(self.queue[1])
-    table.remove(self.queue, 1)
+  function Pop(self)
+    if #self.queue > 0 then
+      print("calling")
+      Auctionator.AH.internals.throttling:Call(self.queue[1])
+      table.remove(self.queue, 1)
+    end
   end
-end
+
+  function Mixin:Push(func)
+    table.insert(self.queue, func)
+
+    if Auctionator.AH.internals.throttling.searchReady then
+      Pop(self)
+    end
+  end
+
+  function Mixin:ReceiveEvent(event)
+    print(event)
+    Pop(self)
+  end
+
+  return Mixin
+end)()
