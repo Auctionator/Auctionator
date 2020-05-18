bee42da97970b27be84ad8f953bd4702152a5ecc
plusmouse <plusmouse@protonmail.com>
2020-05-18 18:32:13 +0100

    [Fixes #730] Add toggle for alternate scan mode by default (#731)

--- Source/Core/Mixin.lua
+++ Source/Core/Mixin.lua
@@ -14,7 +14,11 @@ local function InitializeFullScanFrame()
   else
     frame = Auctionator.State.FullScanFrameRef
   end
-  if Auctionator.Config.Get(Auctionator.Config.Options.AUTOSCAN) then
+
+  if (
+    Auctionator.Config.Get(Auctionator.Config.Options.AUTOSCAN) and
+    not Auctionator.Config.Get(Auctionator.Config.Options.ALTERNATE_SCAN_MODE)
+  ) then
     frame:InitiateScan()
   end
 end
@@ -32,6 +36,13 @@ local function InitializeIncrementalScanFrame()
     Auctionator.State.IncrementalScanFrameRef = frame
   else
     frame = Auctionator.State.IncrementalScanFrameRef
+  end
+
+  if (
+    Auctionator.Config.Get(Auctionator.Config.Options.AUTOSCAN) and
+    Auctionator.Config.Get(Auctionator.Config.Options.ALTERNATE_SCAN_MODE)
+  ) then
+    frame:InitiateScan()
   end
 end
 
