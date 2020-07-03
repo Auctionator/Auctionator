53ddee69244dc66b920f5ac3a62c67838bfea908
plusmouse <plusmouse@protonmail.com>
2020-07-03 01:19:53 +0100

    Change from Shift+Click to just Click

--- Source/Tabs/DataProviders/SearchProvider.lua
+++ Source/Tabs/DataProviders/SearchProvider.lua
@@ -135,7 +135,7 @@ function SearchProviderMixin:ProcessCommodityResults(itemID)
     table.insert(entries, entry)
   end
 
-  if anyOwnedCannotCancel and Auctionator.Config.Get(Auctionator.Config.Options.SELLING_SHIFT_CANCEL) then
+  if anyOwnedCannotCancel and Auctionator.Config.Get(Auctionator.Config.Options.SELLING_CLICK_CANCEL) then
     C_AuctionHouse.QueryOwnedAuctions({})
   end
 
@@ -169,7 +169,7 @@ function SearchProviderMixin:ProcessItemResults(itemKey)
     table.insert(entries, entry)
   end
 
-  if anyOwnedCannotCancel and Auctionator.Config.Get(Auctionator.Config.Options.SELLING_SHIFT_CANCEL) then
+  if anyOwnedCannotCancel and Auctionator.Config.Get(Auctionator.Config.Options.SELLING_CLICK_CANCEL) then
     Auctionator.AH.QueryOwnedAuctions({})
   end
 
