f1fd37edd07d60a09c5d58fbedba6a76d57fe04b
plusmouse <plusmouse@protonmail.com>
2020-08-15 19:10:56 +0100

    [Fixes #640] Clicking on a shopping list result opens the buy screen
    Previously it would show an extra screen, requiring an extra click

--- Source/Tabs/ResultsListing/Mixins/ShoppingListResultsRow.lua
+++ Source/Tabs/ResultsListing/Mixins/ShoppingListResultsRow.lua
@@ -1,7 +1,39 @@
 AuctionatorShoppingListResultsRowMixin = CreateFromMixins(AuctionatorResultsRowTemplateMixin)
 
+local hasModifiedBackButton = false
+local wasOnShoppingTab = false
+
+local function ModifyBuyBackButton()
+  -- Modify click handler to return to the shopping tab
+  hooksecurefunc(AuctionHouseCommoditiesBackButtonMixin, "OnClick",
+    function()
+      if wasOnShoppingTab then
+        AuctionatorTabs_ShoppingLists:Click()
+      end
+    end
+  )
+  AuctionHouseFrame.CommoditiesBuyFrame.BackButton:SetScript("OnClick", AuctionHouseCommoditiesBackButtonMixin.OnClick)
+  AuctionHouseFrame.ItemBuyFrame.BackButton:SetScript("OnClick", AuctionHouseCommoditiesBackButtonMixin.OnClick)
+
+  -- Detection of whether the user was on the "Shopping" tab. Toggle
+  -- wasOnShoppingTab to true after the button displays.
+  local function OnShow(self)
+    wasOnShoppingTab = false
+  end
+  AuctionHouseFrame.CommoditiesBuyFrame.BackButton:SetScript("OnShow", OnShow)
+  AuctionHouseFrame.ItemBuyFrame.BackButton:SetScript("OnShow", OnShow)
+end
+
 function AuctionatorShoppingListResultsRowMixin:OnClick(...)
   Auctionator.Debug.Message("AuctionatorShoppingListResultsRowMixin:OnClick()")
+
+  -- Modify the buy screen back button so that it returns to the "Shopping" tab
+  -- when clicked; checks as to whether the user was on the "Shopping" tab and
+  -- makes the button work normally (not going to the "Shopping" tab) otherwise
+  if not hasModifiedBackButton then
+    hasModifiedBackButton = true
+    ModifyBuyBackButton()
+  end
 
   if IsModifiedClick("DRESSUP") then
     AuctionHouseBrowseResultsFrameMixin.OnBrowseResultSelected({}, self.rowData)
@@ -9,7 +41,10 @@ function AuctionatorShoppingListResultsRowMixin:OnClick(...)
   else
     AuctionatorResultsRowTemplateMixin.OnClick(self, ...)
 
-    AuctionHouseFrameBuyTab:Click()
-    C_AuctionHouse.SearchForItemKeys({ self.rowData.itemKey }, {sortOrder = 1, reverseSort = false})
+    AuctionHouseFrame:SelectBrowseResult(self.rowData)
+
+    -- Switch state _after_ the button is shown. This is enough to check whether
+    -- the user was on the "Shopping" tab or not.
+    wasOnShoppingTab = true
   end
 end
