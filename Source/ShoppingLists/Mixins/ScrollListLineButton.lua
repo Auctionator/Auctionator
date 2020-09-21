f48ee0c97fbaee5ad48d90aa3a40943ecf72d7df
plusmouse <plusmouse@protonmail.com>
2020-09-21 21:14:10 +0100

    Tooltips for "Edit Item" and "Delete" for shopping list items

--- Source/ShoppingLists/Mixins/ScrollListLineButton.lua
+++ Source/ShoppingLists/Mixins/ScrollListLineButton.lua
@@ -6,8 +6,18 @@ end
 function AuctionatorScrollListLineButtonMixin:OnEnter()
   if self:GetParent():IsEnabled() then
     self.hoverTexture:Show()
+
+    if self.tooltipTitleText ~= nil then
+      GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+      GameTooltip:SetText(self.tooltipTitleText, 0.9, 1.0, 1.0)
+      GameTooltip:Show()
+    end
   end
 end
 function AuctionatorScrollListLineButtonMixin:OnLeave()
   self.hoverTexture:Hide()
+
+  if self.tooltipTitleText ~= nil then
+    GameTooltip:Hide()
+  end
 end
