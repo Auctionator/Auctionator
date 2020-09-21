ee8bb0c7393cf4f4ae5d13e099688a8bfaeea9d6
plusmouse <plusmouse@protonmail.com>
2020-09-21 22:14:38 +0100

    [Fixes #790] Selling tab: Fix gaps between the scrollbars and content

--- Source/Tabs/Selling/Mixins/Main.lua
+++ Source/Tabs/Selling/Mixins/Main.lua
@@ -16,11 +16,13 @@ function AuctionatorSellingTabMixin:ApplyHiding()
   if not Auctionator.Config.Get(Auctionator.Config.Options.SHOW_SELLING_PRICE_HISTORY) then
     self.HistoricalPriceProvider:Hide()
     self.HistoricalPriceListing:Hide()
+    self.HistoricalPriceInset:Hide()
     self.CurrentItemListing:SetPoint("BOTTOM", self, "BOTTOM")
   end
 
   if not Auctionator.Config.Get(Auctionator.Config.Options.SHOW_SELLING_BAG) then
     self.BagListing:Hide()
+    self.BagInset:Hide()
     self.CurrentItemListing:SetPoint("LEFT", self, "LEFT", 10, -5)
   end
 end
