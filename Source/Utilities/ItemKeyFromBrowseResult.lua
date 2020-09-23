d71b35b7245b013d09140e299f8bfba5ad1684b8
plusmouse <plusmouse@protonmail.com>
2020-09-23 22:22:58 +0100

    updated incremental scan

--- Source/Utilities/ItemKeyFromBrowseResult.lua
+++ Source/Utilities/ItemKeyFromBrowseResult.lua
@@ -1,6 +1,8 @@
 function Auctionator.Utilities.ItemKeyFromBrowseResult(result)
     if(result.itemKey.battlePetSpeciesID ~= 0) then
       return "p:" .. tostring(result.itemKey.battlePetSpeciesID)
+    elseif result.itemKey.itemLevel ~= 0 then
+      return "gear:" .. result.itemKey.itemID .. ":" .. result.itemKey.itemLevel
     else
       return tostring(result.itemKey.itemID)
     end
