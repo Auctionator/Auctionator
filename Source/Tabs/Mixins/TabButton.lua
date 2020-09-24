a13d9315ff3f680fcf160f5b690809b3846ff151
plusmouse <plusmouse@protonmail.com>
2020-09-24 20:20:52 +0100

    Simplify the code, again. Now working on the PTR

--- Source/Tabs/Mixins/TabButton.lua
+++ Source/Tabs/Mixins/TabButton.lua
@@ -1,6 +1,8 @@
 AuctionatorTabMixin = {}
 
 function AuctionatorTabMixin:OnLoad()
+  self:RegisterEvent("DISPLAY_SIZE_CHANGED")
+
   Auctionator.Debug.Message("AuctionatorTabMixin:OnLoad()")
 
   if self.frameTemplate == nil then
@@ -9,13 +11,11 @@ function AuctionatorTabMixin:OnLoad()
   if self.displayModeKey == nil then
     error("A displayModeKey is required for Auctionator to initialize this tab")
   end
-  if self.ahTabIndex == nil or type(self.ahTabIndex) ~= "number" then
-    error("A numerical ahTabIndex is required for Auctionator to initialize this tab")
-  end
   if self.ahTitle == nil then
     self.ahTitle = "Auctionator"
   end
 
+  self:SetPoint("LEFT", AuctionHouseFrame.Tabs[#AuctionHouseFrame.Tabs - 1], "RIGHT", -15, 0)
   PanelTemplates_DeselectTab(self)
 
   -- Create this tab's frame
@@ -27,19 +27,7 @@ function AuctionatorTabMixin:OnLoad()
   )
   self.frameRef:Hide()
 
-  AuctionHouseFrame.tabsForDisplayMode[self.displayModeKey] = self.ahTabIndex
-
-  self:SetPoint("LEFT", AuctionHouseFrame.Tabs[#AuctionHouseFrame.Tabs - 1], "RIGHT", -15, 0)
-end
-
-function AuctionatorTabMixin:Selected()
-  PanelTemplates_SetTab(AuctionHouseFrame, self)
-  PanelTemplates_SelectTab(self)
-
-  AuctionHouseFrame:SetTitle(self.ahTitle)
-end
-
-function AuctionatorTabMixin:DeselectTab()
-  PanelTemplates_DeselectTab(self)
-  self.frameRef:Hide()
+  AuctionHouseFrameDisplayMode[self.displayModeKey] = self.displayMode
+  AuctionHouseFrame.tabsForDisplayMode[self.displayMode] = #AuctionHouseFrame.Tabs
+  PanelTemplates_SetNumTabs(AuctionHouseFrame, #AuctionHouseFrame.Tabs)
 end
