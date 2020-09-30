d82b614d9850ca5815e8da2957614f89623dacf8
plusmouse <plusmouse@protonmail.com>
2020-09-30 15:10:23 +0100

    Fix addon options sometimes not opening correctly when clicking the button

--- Source/Tabs/Auctionator/Mixins/Main.lua
+++ Source/Tabs/Auctionator/Mixins/Main.lua
@@ -3,3 +3,8 @@ AuctionatorConfigTabMixin = {}
 function AuctionatorConfigTabMixin:OnLoad()
   Auctionator.Debug.Message("AuctionatorConfigTabMixin:OnLoad()")
 end
+
+function AuctionatorConfigTabMixin:OpenOptions()
+  InterfaceOptionsFrame:Show()
+  InterfaceOptionsFrame_OpenToCategory(AUCTIONATOR_L_CONFIG_BASIC_OPTIONS_CATEGORY)
+end
