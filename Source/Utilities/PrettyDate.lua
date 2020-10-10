3fbeb8930e14c2f7026b8683d770aaa124ab8767
plusmouse <plusmouse@protonmail.com>
2020-10-10 13:16:47 +0100

    Custom date format for korean locales

--- Source/Utilities/PrettyDate.lua
+++ Source/Utilities/PrettyDate.lua
@@ -1,9 +1,19 @@
 function Auctionator.Utilities.PrettyDate(when)
   local details = date("*t", when)
 
-  return
-    Auctionator.Locales.Apply("DAY_"..tostring(details.wday)) ..
-    ", " ..
-    Auctionator.Locales.Apply("MONTH_"..tostring(details.month)) ..
-    " " .. details.day
+  local weekDay = Auctionator.Locales.Apply("DAY_"..tostring(details.wday))
+
+  if GetLocale() == "koKR" then
+    -- Korean date format
+    -- Prints, for 25th February 2020, "2020.02.25 [Tuesday]"
+    return date("%Y.%m.%d", when) .. " [" .. weekDay .. "]"
+
+  else
+    -- Default (English) date format
+    -- Prints, for 25th February 2020, "Tuesday, February 25"
+    return
+      weekDay ..  ", " ..
+      Auctionator.Locales.Apply("MONTH_"..tostring(details.month)) ..
+      " " .. details.day
+    end
 end
