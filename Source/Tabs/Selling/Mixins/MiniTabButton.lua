3291a41c36ae44c896d442429580b4762ad58ec1
plusmouse <plusmouse@protonmail.com>
2020-12-09 18:14:16 +0000

    whitespace changes

--- Source/Tabs/Selling/Mixins/MiniTabButton.lua
+++ Source/Tabs/Selling/Mixins/MiniTabButton.lua
@@ -4,19 +4,19 @@ local TAB_PADDING = 20;
 AuctionatorSellingMiniTabButtonMixin = {}
 
 function AuctionatorSellingMiniTabButtonMixin:OnLoad()
-	self.LeftDisabled:SetPoint("TOPLEFT");
-	self.deselectedTextY = 6;
-	self.selectedTextY = 2;
+  self.LeftDisabled:SetPoint("TOPLEFT")
+  self.deselectedTextY = 6
+  self.selectedTextY = 2
 end
 
 function AuctionatorSellingMiniTabButtonMixin:OnShow()
-	local absoluteSize = nil;
-	PanelTemplates_TabResize(self, TAB_PADDING, absoluteSize, MIN_TAB_WIDTH);
+  local absoluteSize = nil
+  PanelTemplates_TabResize(self, TAB_PADDING, absoluteSize, MIN_TAB_WIDTH)
 end
 
 function AuctionatorSellingMiniTabButtonMixin:OnClick()
   PanelTemplates_Tab_OnClick(self, self:GetParent())
   self:GetParent():SetView(self:GetID())
 
-	PlaySound(SOUNDKIT.IG_CHARACTER_INFO_TAB);
+  PlaySound(SOUNDKIT.IG_CHARACTER_INFO_TAB)
 end
