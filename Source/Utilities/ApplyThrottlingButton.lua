92e4144ad563192bfb50f58c5f38ee219269ea36
plusmouse <plusmouse@protonmail.com>
2021-02-11 16:59:16 +0000

    Remove dead code

--- Source/Utilities/ApplyThrottlingButton.lua
+++
@@ -1,8 +0,0 @@
-function Auctionator.Utilities.ApplyThrottlingButton(button, isThrottling)
-  button:SetEnabled(not isThrottling)
-  if isThrottling then
-    button:SetTooltip("Throttled. Waiting for the server.")
-  else
-    button:SetTooltip("")
-  end
-end
