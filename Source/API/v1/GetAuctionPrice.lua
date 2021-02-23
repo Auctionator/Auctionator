bb4668151f41e281f0880442374320e5e2798068
plusmouse <plusmouse@protonmail.com>
2021-02-23 09:42:51 +0000

    Update API to use item level price data when possible without a callback

--- Source/API/v1/GetAuctionPrice.lua
+++ Source/API/v1/GetAuctionPrice.lua
@@ -21,7 +21,18 @@ function Auctionator.API.v1.GetAuctionPriceByItemLink(callerID, itemLink)
     )
   end
 
-  return Auctionator.Database:GetPrice(
-    Auctionator.Utilities.BasicDBKeyFromLink(itemLink)
-  )
+  local dbKeys = nil
+  -- Use that the callback is called immediately (and populates dbKeys) if the
+  -- item info for item levels is available now.
+  Auctionator.Utilities.DBKeyFromLink(itemLink, function(dbKeysCallback)
+    dbKeys = dbKeysCallback
+  end)
+
+  if dbKeys then
+    return Auctionator.Database:GetFirstPrice(dbKeys)
+  else
+    return Auctionator.Database:GetPrice(
+      Auctionator.Utilities.BasicDBKeyFromLink(itemLink)
+    )
+  end
 end
