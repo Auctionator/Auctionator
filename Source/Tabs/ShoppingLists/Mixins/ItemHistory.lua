310034969e792f2552314175e1e6d262d284e39d
plusmouse <plusmouse@protonmail.com>
2021-07-21 18:26:40 +0100

    Add button to put item price history to the side of the shopping tab

--- Source/Tabs/ShoppingLists/Mixins/ItemHistory.lua
+++ Source/Tabs/ShoppingLists/Mixins/ItemHistory.lua
@@ -5,6 +5,7 @@ function AuctionatorItemHistoryFrameMixin:Init()
   self.ResultsListing:Init(self.DataProvider)
 
   Auctionator.EventBus:Register(self, { Auctionator.ShoppingLists.Events.ShowHistoricalPrices })
+  self.isDocked = false
 end
 
 function AuctionatorItemHistoryFrameMixin:OnShow()
@@ -29,6 +30,21 @@ function AuctionatorItemHistoryFrameMixin:ReceiveEvent(event, itemInfo)
   if event == Auctionator.ShoppingLists.Events.ShowHistoricalPrices then
     self.Title:SetText(AUCTIONATOR_L_X_PRICE_HISTORY:format(itemInfo.name))
   end
+end
+
+function AuctionatorItemHistoryFrameMixin:OnDockDialogClicked()
+  self:ClearAllPoints()
+  if self.isDocked then
+    self:SetPoint("CENTER", self:GetParent(), "CENTER")
+    --Reset flipping
+    self.Dock.Arrow:SetTexCoord(0, 1, 0, 1)
+  else
+    self:SetPoint("LEFT", AuctionHouseFrame, "RIGHT")
+    --Flip the texture to point back in
+    self.Dock.Arrow:SetTexCoord(1, 0, 0, 1)
+  end
+
+  self.isDocked = not self.isDocked
 end
 
 function AuctionatorItemHistoryFrameMixin:OnCloseDialogClicked()
