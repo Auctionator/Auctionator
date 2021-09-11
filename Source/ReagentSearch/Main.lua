50f1706a31f4dd35d6587084b3c2f01c5e91384c
plusmouse <plusmouse@protonmail.com>
2021-09-11 12:27:51 +0100

    [Fixes #1059] Error when visiting a vendor in Torghast

--- Source/ReagentSearch/Main.lua
+++ Source/ReagentSearch/Main.lua
@@ -86,16 +86,18 @@ function Auctionator.ReagentSearch.CacheVendorPrices()
     local itemID = GetMerchantItemID(i)
     if itemID ~= nil then
       local item = Item:CreateFromItemID(itemID)
-      item:ContinueOnItemLoad(function()
-        local price, stack, numAvailable = select(3, GetMerchantItemInfo(i))
-        local itemLink = GetMerchantItemLink(i)
-        local dbKey = Auctionator.Utilities.BasicDBKeyFromLink(itemLink)
-        if dbKey ~= nil and price ~= 0 and numAvailable == -1 then
-          AUCTIONATOR_VENDOR_PRICE_CACHE[dbKey] = price / stack
-        elseif dbKey ~= nil then
-          AUCTIONATOR_VENDOR_PRICE_CACHE[dbKey] = nil
-        end
-      end)
+      if not item:IsItemEmpty() then
+        item:ContinueOnItemLoad(function()
+          local price, stack, numAvailable = select(3, GetMerchantItemInfo(i))
+          local itemLink = GetMerchantItemLink(i)
+          local dbKey = Auctionator.Utilities.BasicDBKeyFromLink(itemLink)
+          if dbKey ~= nil and price ~= 0 and numAvailable == -1 then
+            AUCTIONATOR_VENDOR_PRICE_CACHE[dbKey] = price / stack
+          elseif dbKey ~= nil then
+            AUCTIONATOR_VENDOR_PRICE_CACHE[dbKey] = nil
+          end
+        end)
+      end
     end
   end
 end
