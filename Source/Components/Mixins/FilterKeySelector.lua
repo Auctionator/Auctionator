9f0444d085b1cc08e7189379d268bc2e839a17f5
plusmouse <plusmouse@protonmail.com>
2022-04-02 14:20:57 +0100

    [Fixes #1137, Fixes #1140] Search: Add missing filters (#1144)

--- Source/Components/Mixins/FilterKeySelector.lua
+++ Source/Components/Mixins/FilterKeySelector.lua
@@ -11,13 +11,11 @@ function AuctionatorFilterKeySelectorMixin:OnLoad()
 
   UIDropDownMenu_Initialize(self, function(_, level, menuList)
     if level == 1 then
-      self:InitializePrimaryClasses()
-    elseif level == 2 then
-      self:InitializeSecondaryClasses(menuList)
-    elseif level == 3 then
-      self:InitializeArmorSlots(menuList)
+      self:InitializeLevels(level, AuctionCategories, true)
+    elseif menuList ~= nil then
+      self:InitializeLevels(level, menuList.subCategories, menuList.rootChecked, menuList.prefix)
     end
-  end, "taint prevention")
+  end)
 end
 
 function AuctionatorFilterKeySelectorMixin:GetValue()
@@ -50,96 +48,34 @@ function AuctionatorFilterKeySelectorMixin:EntrySelected(displayText)
   CloseDropDownMenus()
 end
 
-function AuctionatorFilterKeySelectorMixin:InitializePrimaryClasses()
+function AuctionatorFilterKeySelectorMixin:InitializeLevels(level, allCategories, rootChecked, prefix)
+  if allCategories == nil then
+    return
+  end
+
   local name
   local info = UIDropDownMenu_CreateInfo()
+  prefix = prefix or ""
 
   info.hasArrow = true
   info.func = function(_, displayText)
     self:EntrySelected(displayText)
   end
 
-  for _, classId in ipairs(Auctionator.Constants.ValidItemClassIDs) do
-    name = GetItemClassInfo(classId)
-    if name ~= nil then
-      info.text = name
-      info.arg1 = name
+  for _, category in ipairs(allCategories) do 
+    if not category:HasFlag("WOW_TOKEN_FLAG") and not category.implicitFilter then
+      info.hasArrow = category.subCategories ~= nil
+
+      info.text = category.name
+      info.arg1 = prefix .. category.name
+      info.checked = rootChecked and info.text == self.selectedCategory[level]
+
       info.menuList = {
-        name = name,
-        classId = classId,
-        subClasses = Auctionator.AH.GetAuctionItemSubClasses(classId)
+        prefix = info.arg1 .. "/",
+        subCategories = category.subCategories,
+        rootChecked = info.checked
       }
-      if self.selectedCategory[1] ~= nil then
-        info.checked = info.arg1 == self.selectedCategory[1]
-      else
-        info.checked = false
-      end
-
-      UIDropDownMenu_AddButton(info)
+      UIDropDownMenu_AddButton(info, level)
     end
-  end
-end
-
-function AuctionatorFilterKeySelectorMixin:InitializeSecondaryClasses(menuList)
-  local name
-  local info = UIDropDownMenu_CreateInfo()
-
-  info.func = function(_, displayText)
-    self:EntrySelected(displayText)
-  end
-
-  for _, subClassId in ipairs(menuList.subClasses) do
-    name = GetItemSubClassInfo(menuList.classId, subClassId)
-
-    info.text = name
-    info.arg1 = menuList.name .. "/" .. name
-
-    if menuList.classId == Enum.ItemClass.Armor then
-      info.hasArrow = true
-      info.menuList = {
-        name = menuList.name .. "/" .. name,
-        classId = menuList.classId,
-        subClassId = subClassId,
-        slots = Auctionator.Constants.INVENTORY_TYPE_IDS
-      }
-    end
-
-    if self.selectedCategory[2] ~= nil then
-      info.checked = info.arg1 == (self.selectedCategory[1] .. "/" .. self.selectedCategory[2])
-    else
-      info.checked = false
-    end
-
-    UIDropDownMenu_AddButton(info, 2)
-  end
-end
-
-function AuctionatorFilterKeySelectorMixin:InitializeArmorSlots(menuList)
-  local name
-  local info = UIDropDownMenu_CreateInfo()
-
-  info.func = function(_, displayText)
-    self:EntrySelected(displayText)
-  end
-
-  for _, armorSlotId in ipairs(Auctionator.Constants.INVENTORY_TYPE_IDS) do
-    name = GetItemInventorySlotInfo(armorSlotId)
-
-    info.text = name
-    info.arg1 = menuList.name .. "/" .. name
-
-    info.value = {
-      classId = menuList.classId,
-      subClassId = menuList.subClassId,
-      armorSlotId = armorSlotId
-    }
-
-    if self.selectedCategory[3] ~= nil then
-      info.checked = info.arg1 == (self.selectedCategory[1] .. "/" .. self.selectedCategory[2] .. "/" .. self.selectedCategory[3])
-    else
-      info.checked = false
-    end
-
-    UIDropDownMenu_AddButton(info, 3)
   end
 end
--- Source/Search/Categories.lua
+++ Source/Search/Categories.lua
@@ -1,128 +1,30 @@
---
---  Auctionator.Search.Categories is an empty table on load, need to populate
---  with the possible categories
---
---  Here's what one entry looks like:
---  {
---    classID    = integer (corresponding to Auctionator.Constants.ValidItemClassIDs )
---    name       = string  (resolved by GetItemClassInfo( classID ))
---    category     = table   (new QueryAuctionItems categoryData format, { classID, subClassID (nil), inventoryType (nil) } )
---    subClasses = {
---      classID  = integer (subClassID)
---      name     = string  (resolved by GetItemSubClassInfo( subClassID ))
---      category   = table   (new QueryAuctionItems categoryData format, { classID, subClassID, inventoryType? } )
---    }
---  }
+local CategoryLookup = {}
 
-local INVENTORY_TYPE_IDS = Auctionator.Constants.INVENTORY_TYPE_IDS
+local function SaveCategory(categories, prefix)
+  prefix = prefix or ""
 
-Auctionator.Search.Category = {
-  classID = 0,
-  name = Auctionator.Constants.CategoryDefault,
-  key = 0,
-  parentKey = nil,
-  category = {},
-  subClasses = {}
-}
+  for _, c in ipairs(categories) do
+    local currentName = prefix .. c.name
+    CategoryLookup[currentName] = c.filters
 
-function Auctionator.Search.Category:new( options )
-  options = options or {}
-  setmetatable( options, self )
-  self.__index = self
-
-  return options
-end
-
---Given a key and category (classID and subClassID supplied, assumed to be for
---armor), creates a new category for each possible inventory slot.
---Returns array of new categories
-local function GenerateArmorInventorySlots(parentKey, parentCategory)
-  local inventorySlots = {}
-  for index = 1, #INVENTORY_TYPE_IDS do
-    local name = GetItemInventorySlotInfo(INVENTORY_TYPE_IDS[index])
-
-    local category = {
-      classID = parentCategory.classID,
-      subClassID = parentCategory.subClassID,
-      inventoryType = INVENTORY_TYPE_IDS[index],
-    }
-    local subSubClass = Auctionator.Search.Category:new({
-      classID = INVENTORY_TYPE_IDS[index],
-      name = name,
-      key = parentKey .. [[/]] .. name,
-      parentKey = parentKey,
-      category = { category }
-    })
-
-    table.insert( inventorySlots, subSubClass )
-  end
-  return inventorySlots
-end
-
-local function GenerateSubClasses( classID, parentKey )
-  local subClassesTable = Auctionator.AH.GetAuctionItemSubClasses( classID )
-  local subClasses = {}
-
-  for index = 1, #subClassesTable do
-    local subClassID = subClassesTable[ index ]
-    local name = GetItemSubClassInfo( classID, subClassID )
-
-    local category = { classID = classID, subClassID = subClassID }
-    local subClass = Auctionator.Search.Category:new({
-      classID = subClassID,
-      name = name,
-      key = parentKey .. [[/]] .. name,
-      parentKey = parentKey,
-      category = { category }
-    })
-
-    table.insert( subClasses, subClass )
-
-    --Armor special case, adds inventory slot categories
-    if classID == Enum.ItemClass.Armor then
-      local inventorySlots = GenerateArmorInventorySlots(subClass.key, category)
-      for _, slot in ipairs(inventorySlots) do
-        table.insert(subClasses, slot)
-      end
+    if c.subCategories ~= nil then
+      SaveCategory(c.subCategories, currentName .. "/")
     end
   end
-
-  return subClasses
 end
 
-function Auctionator.Search.Categories.Initialize()
-  for _, classID in ipairs( Auctionator.Constants.ValidItemClassIDs ) do
-    local key = GetItemClassInfo( classID )
-    local subClasses = GenerateSubClasses( classID, key )
-    local category = {classID = classID}
+function Auctionator.Search.InitializeCategories()
+  Auctionator.Search.InitializeOldCategories()
 
-    local categoryCategory = Auctionator.Search.Category:new({
-      classID = classID,
-      name = name,
-      key = key,
-      category = {category},
-      subClasses = subClasses
-    })
-
-    table.insert( Auctionator.Search.Categories, categoryCategory )
-  end
-
-  for _, category in ipairs( Auctionator.Search.Categories ) do
-    Auctionator.Search.CategoryLookup[ category.key ] = category
-
-    for i = 1, #category.subClasses do
-      local subCategory = category.subClasses[ i ]
-
-      Auctionator.Search.CategoryLookup[ subCategory.key ] = subCategory
-    end
-  end
+  SaveCategory(AuctionCategories)
 end
 
 function Auctionator.Search.GetItemClassCategories(categoryKey)
-  local lookup = Auctionator.Search.CategoryLookup[categoryKey]
+  local lookup = CategoryLookup[categoryKey]
   if lookup ~= nil then
-    return lookup.category
-  else
-    return {}
+    return lookup
+  elseif categoryKey ~= "" then
+    -- Compatibility with old category format
+    return Auctionator.Search.GetItemClassOldCategories(categoryKey)
   end
 end
