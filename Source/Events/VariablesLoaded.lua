68425853716c6055f259417372e78087723f8c7c
plusmouse <plusmouse@protonmail.com>
2022-05-17 17:59:15 +0100

    Reorganize initialization code

--- Source/Events/VariablesLoaded.lua
+++
@@ -1,7 +0,0 @@
-function Auctionator.Events.VariablesLoaded()
-  Auctionator.Debug.Message("Auctionator.Events.VariablesLoaded")
-
-  Auctionator.Variables.Initialize()
-
-  Auctionator.SlashCmd.Initialize()
-end
---
+++ Source_Classic/Initialize/Main.lua
@@ -0,0 +1,33 @@
+local AUCTIONATOR_EVENTS = {
+  -- AH Window Initialization Events
+  "AUCTION_HOUSE_SHOW",
+}
+
+AuctionatorInitializeClassicMixin = {}
+
+function AuctionatorInitializeClassicMixin:OnLoad()
+  FrameUtil.RegisterFrameForEvents(self, AUCTIONATOR_EVENTS)
+end
+
+function AuctionatorInitializeClassicMixin:OnEvent(event, ...)
+  if event == "AUCTION_HOUSE_SHOW" then
+    self:AuctionHouseShown()
+  end
+end
+
+function AuctionatorInitializeClassicMixin:AuctionHouseShown()
+  Auctionator.Debug.Message("AuctionatorInitializeClassicMixin:AuctionHouseShown()")
+
+  -- Prevents a lot of errors if loaded in retail
+  if AuctionFrame == nil then
+    return
+  end
+
+  Auctionator.AH.Initialize()
+
+  if Auctionator.State.AuctionatorFrame == nil then
+    Auctionator.State.AuctionatorFrame = CreateFrame("FRAME", "AuctionatorAHFrame", AuctionFrame, "AuctionatorAHFrameTemplate")
+  end
+
+  FrameUtil.RegisterFrameForEvents(Auctionator.State.AuctionatorFrame, { "AUCTION_HOUSE_SHOW", "AUCTION_HOUSE_CLOSED" })
+end
