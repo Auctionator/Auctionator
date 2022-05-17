a98fca4739acec457c4103c93a0cb449ea95d24c
plusmouse <plusmouse@protonmail.com>
2022-05-17 18:08:52 +0100

    [Fixes #1112] Shift+Click to search from enchant pane

---
+++ Source_Vanilla/Initialize/Main.lua
@@ -0,0 +1,42 @@
+local AUCTIONATOR_EVENTS = {
+  "CRAFT_SHOW",
+}
+
+AuctionatorInitializeVanillaMixin = {}
+
+function AuctionatorInitializeVanillaMixin:OnLoad()
+  FrameUtil.RegisterFrameForEvents(self, AUCTIONATOR_EVENTS)
+end
+
+function AuctionatorInitializeVanillaMixin:OnEvent(event, ...)
+  if event == "CRAFT_SHOW" then
+    self:CraftShown()
+  end
+end
+
+function AuctionatorInitializeVanillaMixin:CraftShown()
+  Auctionator.Debug.Message("AuctionatorInitializeVanillaMixin::CraftShown()")
+
+  if self.initializedCraftHooks then
+    return
+  end
+
+  local reagentHook = function(self)
+    if IsModifiedClick("CHATLINK") and AuctionatorShoppingListFrame ~= nil and AuctionatorShoppingListFrame:IsVisible() then
+      local name = GetCraftReagentInfo(GetCraftSelectionIndex(), self:GetID())
+
+      if name == nil then
+        return
+      end
+
+      AuctionatorShoppingListFrame.OneItemSearchBox:SetText(name)
+      AuctionatorShoppingListFrame.OneItemSearchButton:Click()
+    end
+  end
+  CraftReagent1:HookScript("OnClick", reagentHook)
+  CraftReagent2:HookScript("OnClick", reagentHook)
+  CraftReagent3:HookScript("OnClick", reagentHook)
+  CraftReagent4:HookScript("OnClick", reagentHook)
+
+  self.initializedCraftHooks = true
+end
