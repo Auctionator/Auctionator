e9586679127864d550ac05e4c68baf9e4ba72733
plusmouse <plusmouse@protonmail.com>
2022-08-17 10:46:41 +0100

    Change default full scan mode to incremental as replicate is too slow in 9.2.7

--- Source_Mainline/Tabs/Auctionator/Mixins/ScanButton.lua
+++ Source_Mainline/Tabs/Auctionator/Mixins/ScanButton.lua
@@ -1,17 +1,5 @@
 AuctionatorScanButtonMixin = {}
 
 function AuctionatorScanButtonMixin:OnClick()
-  if Auctionator.Config.Get(Auctionator.Config.Options.REPLICATE_SCAN) then
-    if not IsControlKeyDown() then
-      Auctionator.State.FullScanFrameRef:InitiateScan()
-    else
-      Auctionator.State.IncrementalScanFrameRef:InitiateScan()
-    end
-  else
-    if not IsControlKeyDown() then
-      Auctionator.State.IncrementalScanFrameRef:InitiateScan()
-    else
-      Auctionator.State.FullScanFrameRef:InitiateScan()
-    end
-  end
+  Auctionator.State.IncrementalScanFrameRef:InitiateScan()
 end
