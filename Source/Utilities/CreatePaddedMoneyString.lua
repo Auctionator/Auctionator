6a63dca69bc9ed43d242a850e03f015b07a4e591
plusmouse <plusmouse@protonmail.com>
2024-01-27 20:29:18 +0000

    Fix inconsistent padding for tooltip gold counts on different scales

--- Source/Utilities/CreatePaddedMoneyString.lua
+++ Source/Utilities/CreatePaddedMoneyString.lua
@@ -10,24 +10,25 @@ local function getGold(amount)
   return (amount - getSilver(amount) * 100 - getCopper(amount)) / 10000
 end
 
-local goldIcon = "|TInterface\\MoneyFrame\\UI-GoldIcon:12:12:4:0|t"
-local silverIcon = "|TInterface\\MoneyFrame\\UI-SilverIcon:12:12:4:0|t"
-local copperIcon = "|TInterface\\MoneyFrame\\UI-CopperIcon:12:12:4:0|t"
-local padding = "  "
+local goldIcon = "|TInterface\\MoneyFrame\\UI-GoldIcon:12:12:0:0|t"
+local silverIcon = "|TInterface\\MoneyFrame\\UI-SilverIcon:12:12:0:0|t"
+local copperIcon = "|TInterface\\MoneyFrame\\UI-CopperIcon:12:12:0:0|t"
+local leftPadding = " "
+local rightPadding = " "
 
 function Auctionator.Utilities.CreatePaddedMoneyString(amount)
   amount = math.floor(amount)
 
   local gold, silver, copper = getGold(amount), getSilver(amount), getCopper(amount)
 
-  local result = copper .. copperIcon
+  local result = copper .. leftPadding .. copperIcon
 
   if (gold ~= 0 or silver ~= 0) and copper < 10 then
     result = "0" .. result
   end
 
   if silver ~= 0 or gold ~= 0 then
-    result = silver .. silverIcon .. padding .. result
+    result = silver .. leftPadding .. silverIcon .. rightPadding .. result
   end
 
   if gold ~= 0 and silver < 10 then
@@ -35,7 +36,7 @@ function Auctionator.Utilities.CreatePaddedMoneyString(amount)
   end
 
   if gold ~= 0 then
-    result = gold .. goldIcon .. padding ..result
+    result = gold .. leftPadding .. goldIcon .. rightPadding ..result
   end
 
   return result
