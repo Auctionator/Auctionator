8a47de650f033ad972c2201a9db6e5153b22840a
plusmouse <plusmouse@protonmail.com>
2024-03-14 14:49:10 +0000

    Fix division by 0 error on beta/ptr WoW builds

--- Source/Utilities/PriceWarningThreshold.lua
+++ Source/Utilities/PriceWarningThreshold.lua
@@ -1,4 +1,7 @@
 function Auctionator.Utilities.PriceWarningThreshold(unitPrice)
+  if unitPrice == 0 then
+    return 0
+  end
   -- Scale the % of the price that counts as too little from 70% at 1g and
   -- down to 30% at higher prices
   local multiplier = 0.3 + 0.4 * math.min(1, 10000 / unitPrice)
