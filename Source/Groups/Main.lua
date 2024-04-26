3554b7bfcf9fadae3b53a42fd46867088881479f
plusmouse <plusmouse@protonmail.com>
2024-04-26 19:20:25 +0100

    [Fixes #1490] Favourites being hidden if previously not set as first group

--- Source/Groups/Main.lua
+++ Source/Groups/Main.lua
@@ -21,7 +21,19 @@ function Auctionator.Groups.Initialize()
     AUCTIONATOR_SELLING_GROUPS.CustomGroups = AUCTIONATOR_SELLING_GROUPS.CustomSections
     AUCTIONATOR_SELLING_GROUPS.CustomSections = nil
   end
-  if AUCTIONATOR_SELLING_GROUPS.CustomGroups[1].name == "FAVOURITES_GROUP" then
+
+  local group1 = AUCTIONATOR_SELLING_GROUPS.CustomGroups[1]
+  if group1.name ~= "FAVOURITES_GROUP" and group1.name ~= "FAVOURITES" then
+    for index, group in ipairs(AUCTIONATOR_SELLING_GROUPS.CustomGroups) do
+      if group.name == "FAVOURITES_GROUP" or group.name == "FAVOURITES" then
+        AUCTIONATOR_SELLING_GROUPS.CustomGroups[index] = group1
+        AUCTIONATOR_SELLING_GROUPS.CustomGroups[1] = group
+        group1 = group
+      end
+    end
+  end
+
+  if group1.name == "FAVOURITES_GROUP" then
     AUCTIONATOR_SELLING_GROUPS.CustomGroups[1].name = "FAVOURITES"
   end
 end
