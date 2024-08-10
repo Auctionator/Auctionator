c48e49edad7b856bf34ff60d2098b44787305063
plusmouse <plusmouse@protonmail.com>
2024-08-10 15:33:29 +0100

    Retail: Fix soulbound items showing "Auction unknown" in tooltips

--- Source/Utilities/IsBound.lua
+++ Source/Utilities/IsBound.lua
@@ -1,5 +1,10 @@
+local unboundConstants = {
+  LE_ITEM_BIND_NONE or Enum.ItemBind.None,
+  LE_ITEM_BIND_ON_EQUIP or Enum.ItemBind.OnEquip,
+  LE_ITEM_BIND_ON_USE or Enum.ItemBind.OnUse,
+}
 function Auctionator.Utilities.IsBound(itemInfo)
   local bindType = itemInfo[Auctionator.Constants.ITEM_INFO.BIND_TYPE]
 
-  return bindType == LE_ITEM_BIND_ON_ACQUIRE or bindType == LE_ITEM_BIND_QUEST
+  return tIndexOf(unboundConstants, bindType) == nil
 end
