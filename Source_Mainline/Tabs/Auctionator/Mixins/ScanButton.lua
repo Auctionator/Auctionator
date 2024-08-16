ea5508922947abb5532e17c907adf6c77bf413df
plusmouse <plusmouse@protonmail.com>
2024-08-16 14:07:37 +0100

    Retail: Remove Control+Click to alternate scan mode shortcut

--- Source_Mainline/Tabs/Auctionator/Mixins/ScanButton.lua
+++ Source_Mainline/Tabs/Auctionator/Mixins/ScanButton.lua
@@ -2,16 +2,8 @@ AuctionatorScanButtonMixin = {}
 
 function AuctionatorScanButtonMixin:OnClick()
   if Auctionator.Config.Get(Auctionator.Config.Options.REPLICATE_SCAN) then
-    if not IsControlKeyDown() then
-      Auctionator.State.FullScanFrameRef:InitiateScan()
-    else
-      Auctionator.State.IncrementalScanFrameRef:InitiateScan()
-    end
+    Auctionator.State.FullScanFrameRef:InitiateScan()
   else
-    if not IsControlKeyDown() then
-      Auctionator.State.IncrementalScanFrameRef:InitiateScan()
-    else
-      Auctionator.State.FullScanFrameRef:InitiateScan()
-    end
+    Auctionator.State.IncrementalScanFrameRef:InitiateScan()
   end
 end
