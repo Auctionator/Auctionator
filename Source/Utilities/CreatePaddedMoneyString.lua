015a27510b5682addbac117137c10c0c93065d15
Murilo Mielke <murilo.eng@gmail.com>
2024-09-18 21:09:55 -0300

    Utilities: adding optional parameters on CreatePaddedMoneyString to not show silver/copper

--- Source/Utilities/CreatePaddedMoneyString.lua
+++ Source/Utilities/CreatePaddedMoneyString.lua
@@ -16,27 +16,41 @@ local copperIcon = "|TInterface\\MoneyFrame\\UI-CopperIcon:12:12:0:0|t"
 local leftPadding = " "
 local rightPadding = " "
 
-function Auctionator.Utilities.CreatePaddedMoneyString(amount)
+function Auctionator.Utilities.CreatePaddedMoneyString(amount, showSilver, showCopper)
   amount = math.floor(amount)
 
   local gold, silver, copper = getGold(amount), getSilver(amount), getCopper(amount)
 
-  local result = copper .. leftPadding .. copperIcon
+  local paddedCopper = copper
+  if copper < 10 then
+    paddedCopper = "0" .. copper
+  end
 
-  if (gold ~= 0 or silver ~= 0) and copper < 10 then
-    result = "0" .. result
+  local paddedSilver = silver
+  if silver < 10 then
+    paddedSilver = "0" .. silver
+  end
+  
+  local result = ''
+  
+  if showCopper == true or showCopper == nil then
+    result = paddedCopper .. rightPadding .. copperIcon
   end
 
   if silver ~= 0 or gold ~= 0 then
-    result = silver .. leftPadding .. silverIcon .. rightPadding .. result
-  end
-
-  if gold ~= 0 and silver < 10 then
-    result = "0" .. result
+    if showSilver == true or showSilver == nil then
+      if result ~= '' then
+        result = rightPadding .. result
+      end
+      result = paddedSilver .. leftPadding .. silverIcon .. result
+    end
   end
 
   if gold ~= 0 then
-    result = gold .. leftPadding .. goldIcon .. rightPadding ..result
+    if result ~= '' then
+      result = rightPadding .. result
+    end
+    result = gold .. leftPadding .. goldIcon .. result
   end
 
   return result
