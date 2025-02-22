bfc2a8a73f2844d2a0d4c7653b8f185c31795d2b
plusmouse <plusmouse@protonmail.com>
2025-02-22 20:48:35 +0000

    Cata: Show coppers in AH listings

--- Source_ModernAH/Components/Mixins/MoneyInput.lua
+++ Source_ModernAH/Components/Mixins/MoneyInput.lua
@@ -5,11 +5,20 @@ function AuctionatorConfigMoneyInputMixin:OnLoad()
     self.Label:SetText(self.labelText)
   end
 
+  if not self.MoneyInput.hideCopper then
+    self.MoneyInput:SetWidth(190)
+  end
+
+  self.MoneyInput.CopperBox:SetScript("OnEnter", function() self:OnEnter() end)
+  self.MoneyInput.CopperBox:SetScript("OnLeave", function() self:OnLeave() end)
   self.MoneyInput.SilverBox:SetScript("OnEnter", function() self:OnEnter() end)
   self.MoneyInput.SilverBox:SetScript("OnLeave", function() self:OnLeave() end)
   self.MoneyInput.GoldBox:SetScript("OnEnter", function() self:OnEnter() end)
   self.MoneyInput.GoldBox:SetScript("OnLeave", function() self:OnLeave() end)
 
+  self.MoneyInput.CopperBox:SetScript("OnEnterPressed", function()
+    Auctionator.Components.ReportEnterPressed()
+  end)
   self.MoneyInput.SilverBox:SetScript("OnEnterPressed", function()
     Auctionator.Components.ReportEnterPressed()
   end)
