fbbb0b19267bb0d41de4f64af7a42275b0ce80e0
plusmouse <plusmouse@protonmail.com>
2025-04-23 08:31:20 +0100

    [Fixes #1673, Fixes #1672] Fix item link change breaking Selling searches

--- Source/Utilities/GetQualityColorFromLink.lua
+++ Source/Utilities/GetQualityColorFromLink.lua
@@ -1,3 +1,3 @@
 function Auctionator.Utilities.GetQualityColorFromLink(itemLink)
-  return string.match(itemLink, "|c(........)|")
+  return string.match(itemLink, "|c(nIQ%d+:)|") or string.match(itemLink, "|c(........)|")
 end
