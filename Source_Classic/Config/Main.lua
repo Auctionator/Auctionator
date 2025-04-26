13889f6f84249d989c3f801690cf829af2997852
plusmouse <plusmouse@protonmail.com>
2025-04-26 18:01:37 +0100

    Cata Classic: Restore Prospect and Mill tooltips (show estimated value)

---
+++ Source_Classic/Config/Main.lua
@@ -0,0 +1,5 @@
+Auctionator.Config.Options.PROSPECT_TOOLTIPS = "prospect_tooltips"
+Auctionator.Config.Options.MILL_TOOLTIPS = "mill_tooltips"
+
+Auctionator.Config.Defaults[Auctionator.Config.Options.PROSPECT_TOOLTIPS] = false
+Auctionator.Config.Defaults[Auctionator.Config.Options.MILL_TOOLTIPS] = false
--- Source_ModernAH/Config/Mixins/Tooltips.lua
+++ Source_ModernAH/Config/Mixins/Tooltips.lua
@@ -7,6 +7,13 @@ function AuctionatorConfigTooltipsFrameMixin:OnLoad()
   self.parent = "Auctionator"
 
   self:SetupPanel()
+
+  if not Auctionator.Constants.IsRetail then
+    self.EnchantTooltips:SetText(AUCTIONATOR_L_CONFIG_ENCHANT_GENERIC_TOOLTIP)
+    self.ProspectTooltips:Show()
+    self.MillTooltips:Show()
+    self.AuctionAgeTooltips:SetPoint("TOPLEFT", self.MillTooltips, "BOTTOMLEFT")
+  end
 end
 
 function AuctionatorConfigTooltipsFrameMixin:ShowSettings()
@@ -17,6 +24,11 @@ function AuctionatorConfigTooltipsFrameMixin:ShowSettings()
   self.EnchantTooltips:SetChecked(Auctionator.Config.Get(Auctionator.Config.Options.ENCHANT_TOOLTIPS))
   self.ShiftStackTooltips:SetChecked(Auctionator.Config.Get(Auctionator.Config.Options.SHIFT_STACK_TOOLTIPS))
   self.AuctionAgeTooltips:SetChecked(Auctionator.Config.Get(Auctionator.Config.Options.AUCTION_AGE_TOOLTIPS))
+
+  if not Auctionator.Constants.IsRetail then
+    self.ProspectTooltips:SetChecked(Auctionator.Config.Get(Auctionator.Config.Options.PROSPECT_TOOLTIPS))
+    self.MillTooltips:SetChecked(Auctionator.Config.Get(Auctionator.Config.Options.MILL_TOOLTIPS))
+  end
 end
 
 function AuctionatorConfigTooltipsFrameMixin:Save()
@@ -29,6 +41,11 @@ function AuctionatorConfigTooltipsFrameMixin:Save()
   Auctionator.Config.Set(Auctionator.Config.Options.ENCHANT_TOOLTIPS, self.EnchantTooltips:GetChecked())
   Auctionator.Config.Set(Auctionator.Config.Options.SHIFT_STACK_TOOLTIPS, self.ShiftStackTooltips:GetChecked())
   Auctionator.Config.Set(Auctionator.Config.Options.AUCTION_AGE_TOOLTIPS, self.AuctionAgeTooltips:GetChecked())
+
+  if not Auctionator.Constants.IsRetail then
+    Auctionator.Config.Set(Auctionator.Config.Options.PROSPECT_TOOLTIPS, self.ProspectTooltips:GetChecked())
+    Auctionator.Config.Set(Auctionator.Config.Options.MILL_TOOLTIPS, self.MillTooltips:GetChecked())
+  end
 end
 
 function AuctionatorConfigTooltipsFrameMixin:Cancel()
